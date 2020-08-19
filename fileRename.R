4e088dbb3dfe433253777c18aaa4c8a0daf553a3
Kate W <magpiedin@gmail.com>
2020-08-19 17:10:50 -0500

    cleanup

--- fileRename.R
+++ fileRename.R
@@ -15,26 +15,40 @@ library(readr)
 library(stringr)
 
 namelist <- read_csv("namelist.csv")
- 
-
-all(file.exists(namelist$oldDNGname))
-all(file.exists(namelist$oldJPGname))
 
 
+# check that filepaths exist & names are correct 
+# NOTE - this can take a while for long lists
+Sys.time()
+namelist$exists <- ""
+namelist$exists <- file.exists(namelist$oldname)
+Sys.time()
+
+
+# rename the files
+# NOTE - test this on a few first, then run in full
 for (i in 1:NROW(namelist)) {
   
   # file.rename(from = namelist$oldDNGname[i], to = namelist$newDNGname[i])
   # 
   # file.rename(from = namelist$oldJPGname[i], to = namelist$newJPGname[i])
 
-  file.rename(from = namelist$newDNGname[i], to = namelist$new2DNGname[i])
-  
-  file.rename(from = namelist$newJPGname[i], to = namelist$new2JPGname[i])
+  file.rename(from = namelist$oldname[i], to = namelist$newname[i])
   
   print(i)
   
 }
 
 
-all(file.exists(namelist$new2DNGname))
-all(file.exists(namelist$new2JPGname))
+# check that filepaths exist & names are correct 
+# NOTE - this can take a while for long lists
+Sys.time()
+namelist$newexists <- ""
+namelist$newexists <- all(file.exists(namelist$newname))
+Sys.time()
+
+# namelist$newDNGexists <- ""
+# namelist$newJPGexists <- ""
+# namelist$newDNGexists <- all(file.exists(namelist$newDNGname))
+# namelist$newJPGexists <- all(file.exists(namelist$newJPGname))
+# Sys.time()
